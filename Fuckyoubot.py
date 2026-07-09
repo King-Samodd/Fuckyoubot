459a927f64a710a4ee11e68db1a69d44744b024e
King-Samodd <babarindesamod0@gmail.com>
2026-07-08 18:30:36 -0700

    Add files via upload

---
+++ Fuckyoubot.py
@@ -0,0 +1,7 @@
+while True:
+    message = input("You: ").strip().lower()
+
+    if message == "good morning":
+        print("Bot: Fuck you.")
+    else:
+        print("Bot: Who raised you? Don't you have any respect?")
